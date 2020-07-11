ad11f1244df5e870b95749af4f1182bea0827e58
Swarnava Sadhukhan <67735707+Swarnava-Sadhukhan@users.noreply.github.com>
2020-07-11 20:02:02 +0530

    Update main.js

--- ToDo List/main.js
+++ ToDo List/main.js
@@ -2,6 +2,7 @@ const container = document.querySelector('.container');
 var inputValue = document.querySelector('.input');
 const add = document.querySelector('.add');
 
+//initial condition
 if(window.localStorage.getItem("todos") == undefined){
      var todos = [];
      window.localStorage.setItem("todos", JSON.stringify(todos));
@@ -10,11 +11,12 @@ if(window.localStorage.getItem("todos") == undefined){
 var todosEX = window.localStorage.getItem("todos");
 var todos = JSON.parse(todosEX);
 
-
+//for each todo iteem
 class item{
 	constructor(name){
 		this.createItem(name);
 	}
+	//item creation
     createItem(name){
     	var itemBox = document.createElement('div');
         itemBox.classList.add('item');
@@ -43,6 +45,7 @@ class item{
 
     }
 
+    //edit item
     edit(input, name, edit1){
         if(input.disabled == true){
            input.disabled = !input.disabled;
@@ -57,6 +60,7 @@ class item{
         }
     }
 
+    //removes item from list
     remove(itemBox, name){
         itemBox.parentNode.removeChild(itemBox);
         let index = todos.indexOf(name);
@@ -72,6 +76,7 @@ window.addEventListener('keydown', (e) => {
 	}
 })
 
+//adds item to the list
 function check(){
 	if(inputValue.value != ""){
 		new item(inputValue.value);
@@ -86,5 +91,3 @@ for (var v = 0 ; v < todos.length ; v++){
     new item(todos[v]);
 }
 
-
-//new item("sport");
